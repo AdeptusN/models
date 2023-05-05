c5969ad0844bab33faeaef359ed2869e948bd20c
EgorProkopov <egorprokopov216@gmail.com>
2023-05-05 17:17:52 +0300

    Add use_sigmoid optional arg

--- adeptus_models/segmentation.py
+++ adeptus_models/segmentation.py
@@ -9,16 +9,16 @@ class UNet(nn.Module):
     """
     UNet model for segmentation with changeable number of layers
     """
-    def __init__(
-            self, in_channels=3, out_channels=1, features=(64, 128, 256, 512)
-    ):
+    def __init__(self, in_channels=3, out_channels=1, features=(64, 128, 256, 512), use_sigmoid=False):
         """
         Args:
             in_channels: Number of channels in the input image
             out_channels: Number of channels in the out mask
             features: tuple of layers activation maps numbers
+            use_sigmoid: if True, there will be sigmoid activation function on the network output
         """
         super(UNet, self).__init__()
+        self.use_sigmoid = use_sigmoid
         self.ups = nn.ModuleList()
         self.downs = nn.ModuleList()
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, return_indices=False)
@@ -48,20 +48,14 @@ class UNet(nn.Module):
             activation_func=nn.ReLU()
         )
         self.classifier = nn.Sequential(
-            # Conv5x5(features[0], features[0], batch_norm=True, dropout=False, activation_func=nn.ReLU()),
             nn.Conv2d(features[0], out_channels, kernel_size=1)
         )
-        self.sigmoid = nn.Sigmoid()  # - откомментить, если используется самописная функция активации
+
+        self.sigmoid = None
+        if self.use_sigmoid:
+            self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        """
-        Forward propagation method of neural network.
-        Args:
-            x: mini-batch of data
-
-        Returns:
-            Result of network working
-        """
         skip_connections = []
 
         for down in self.downs:
@@ -80,7 +74,8 @@ class UNet(nn.Module):
             x = self.ups[i + 1](concat_skip)
 
         out = self.classifier(x)
-        out = self.sigmoid(out)
+        if self.use_sigmoid:
+            out = self.sigmoid(out)
 
         return out
     
