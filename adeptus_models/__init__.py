d83e21007eca74e3884fc414e35d64497e6be9a4
EgorProkopov <egorprokopov216@gmail.com>
2023-05-05 17:18:31 +0300

    add save_model and load_model methods

--- adeptus_models/__init__.py
+++ adeptus_models/__init__.py
@@ -1,3 +1,26 @@
+import torch
+
 from .autoencoder import Autoencoder
 from .segmentation import UNet
 from .unet_autoencoder import UNetAutoencoder
+
+
+def save_model(model, path: str):
+    """
+    Saves model weights
+    Args:
+        model: model to save
+        path: path to save
+    """
+    torch.save(model.state_dict(), path)
+
+
+def load_model(model, path: str):
+    """
+    Loads model weights
+    Args:
+        model: model to load
+        path: weights path
+    """
+    model.load_state_dict(torch.load(path))
+    return model
